0018321a8e47fbdc5de40d8c0c5c3e976ceed109
zwarag <harrys.kavan@pm.me>
2020-12-25 15:53:54 +0100

    init level navigator component

---
+++ src/app/level-navigator/level-navigator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-level-navigator',
+  templateUrl: './level-navigator.component.html',
+  styleUrls: ['./level-navigator.component.scss']
+})
+export class LevelNavigatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
