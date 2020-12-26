26bbeadd3613a6e9230063476cca846462b12ae7
zwarag <harrys.kavan@pm.me>
2020-12-26 14:24:45 +0100

    start better route level navigator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HelloWorldComponent } from "./hello-world/hello-world.component";
+import { Routes, RouterModule, UrlSegment } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { LevelNavigatorComponent } from './level-navigator/level-navigator.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HelloWorldComponent },
-  { path: 'login', component: LoginComponent }
+  { path: '', pathMatch: 'full', redirectTo: 'discipline' },
+  { path: 'login', component: LoginComponent },
+  { matcher: (segments: UrlSegment[]) => {
+      console.log('segments', segments);
+      return null;
+    }, component: LevelNavigatorComponent },
+  { path: 'discipline/:discipline/:studycourse/:lecture/:file', component: LevelNavigatorComponent },
 ];
 
 @NgModule({
