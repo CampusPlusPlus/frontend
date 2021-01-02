754c54bef2412a848f731a9afbabce087c756895
zwarag <harrys.kavan@pm.me>
2021-01-02 12:59:11 +0100

    snap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, UrlSegment } from '@angular/router';
+import { Route, RouterModule, Routes, UrlSegment, UrlSegmentGroup } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LevelNavigatorComponent } from './level-navigator/level-navigator.component';
+import { FileDetailComponent } from './file-detail/file-detail.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'discipline' },
   { path: 'login', component: LoginComponent },
-  { matcher: (segments: UrlSegment[]) => {
+  {
+    matcher: (segments: UrlSegment[], group: UrlSegmentGroup, route: Route) => {
       console.log('segments', segments);
-      return null;
-    }, component: LevelNavigatorComponent },
-  { path: 'discipline/:discipline/:studycourse/:lecture/:file', component: LevelNavigatorComponent },
+      console.log('group', group);
+      console.log('route', route);
+      if (segments.length === 0 || segments.length > 4) {
+        return null;
+      }
+      if (segments[0].toString() !== 'discipline') {
+        return null;
+      }
+      return { consumed: segments };
+    }, component: LevelNavigatorComponent
+  },
+  {
+    matcher: (segments: UrlSegment[], group: UrlSegmentGroup, route: Route) => {
+      console.log('segments', segments);
+      console.log('group', group);
+      console.log('route', route);
+      if (segments.length === 0 || segments.length > 5) {
+        return null;
+      }
+      if (segments[0].toString() !== 'discipline') {
+        return null;
+      }
+      return { consumed: segments };
+    }, component: FileDetailComponent
+  },
 ];
 
 @NgModule({
