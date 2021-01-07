d45f708fe1f6411c8ad12f221615c95cf83699aa
zwarag <harrys.kavan@protonmail.com>
2021-01-07 17:02:24 +0100

    refactor tags and fix creating an existing tag

--- src/app/file-upload-form/tags/tags.component.ts
+++ src/app/file-upload-form/tags/tags.component.ts
@@ -1,12 +1,12 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {FormControl, FormGroup} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {MatAutocomplete, MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
-import {map, startWith} from 'rxjs/operators';
-import {MatChipInputEvent} from '@angular/material/chips';
-import {TagService} from '../../shared/services/tag.service';
-import {Tag} from '../../shared/models/Tag';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { map, startWith } from 'rxjs/operators';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { TagService } from '../../shared/services/tag.service';
+import { Tag } from '../../shared/models/Tag';
 
 @Component({
   selector: 'app-tags',
@@ -14,7 +14,7 @@ import {Tag} from '../../shared/models/Tag';
   styleUrls: ['./tags.component.scss']
 })
 export class TagsComponent implements OnInit {
-  @ViewChild('tagInput', {static: false}) tagInput: ElementRef<HTMLInputElement>;
+  @ViewChild('tagInput', { static: false }) tagInput: ElementRef<HTMLInputElement>;
   @Input() tags: string[];
   tagNames: string[] = [];
   selectable = true;
@@ -22,18 +22,19 @@ export class TagsComponent implements OnInit {
   separatorKeysCodes: number[] = [ENTER, COMMA];
   tagCtrl = new FormControl();
   filteredTags: Observable<string[]>;
-  backendTags: Tag[] = [];
+  allTags: Tag[] = [];
 
   constructor(private tagService: TagService) {
+    this.fetchTags();
     this.filteredTags = this.tagCtrl.valueChanges.pipe(
       startWith(null),
-      map((tag: string | null) => tag ? this._filter(tag) : this.tagNames.slice()));
+      map((userInput: string | null) => userInput ? this._filter(userInput) : this.tagNames.slice()));
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    this.backendTags.forEach(t => {
-      if (this.tagNames.length < this.backendTags.length) {
+    this.allTags.forEach(t => {
+      if (this.tagNames.length < this.allTags.length) {
         this.tagNames.push(t.tagValue);
         console.log(t);
       }
@@ -43,7 +44,6 @@ export class TagsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.initTags();
   }
 
   add(event: MatChipInputEvent): void {
@@ -60,9 +60,11 @@ export class TagsComponent implements OnInit {
       input.value = '';
     }
 
-    this.tagService.createTag$(value).subscribe();
+    if (this.allTags.map(x => x.tagValue).findIndex(x => x === value.toString()) === -1) {
+      this.tagService.createTag$(value).subscribe();
+    }
     this.tagCtrl.setValue(null);
-    this.initTags();
+    this.fetchTags();
   }
 
   remove(tag: string): void {
@@ -83,14 +85,14 @@ export class TagsComponent implements OnInit {
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.initTags();
+    this.fetchTags();
     this.tags.push(event.option.viewValue);
     this.tagInput.nativeElement.value = '';
     this.tagCtrl.setValue(null);
   }
 
-  private initTags(): void {
-    this.backendTags = this.tagService.getAllTags();
+  private fetchTags(): void {
+    this.allTags = this.tagService.getAllTags();
   }
 
 
